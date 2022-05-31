661c68a3cbf3584d78ab2fbac632b7310bc3c1e7
utkasigns <92622325+utkasigns@users.noreply.github.com>
2022-05-31 10:40:20 +0300

    added searching notes

--- app/src/main/java/com/example/felixalarm/activities/NotesActivity.java
+++ app/src/main/java/com/example/felixalarm/activities/NotesActivity.java
@@ -11,9 +11,12 @@ import android.content.AsyncQueryHandler;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.felixalarm.R;
@@ -67,6 +70,27 @@ public class NotesActivity extends AppCompatActivity implements NotesListener {
         notesRecyclerView.setAdapter(notesAdapter);
 
         getNotes(REQUEST_CODE_SHOW_NOTES, false);
+
+        EditText inputSearch = findViewById(R.id.inputSearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                notesAdapter.cancelTimer();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (noteList.size() != 0) {
+                    notesAdapter.searchNotes(editable.toString());
+                }
+            }
+        });
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.nav_notes);
